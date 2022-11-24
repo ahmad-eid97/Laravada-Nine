9d04b2abc1e54344b8094cf9d8b5f89726ae9c9c
civil.ahmad.eid <civil.ahmad.eid@gmail.com>
2022-11-24 16:27:36 +0200

    add data

--- components/blog/AppBlogGallery.vue
+++ components/blog/AppBlogGallery.vue
@@ -3,7 +3,7 @@
         <h3 class="title">Gallery</h3>
         <ul class="blog-gallery">
             <li
-                v-for="(image, index) in media"
+                v-for="(image, index) in blogDetails.gallery"
                 :key="index"
                 style="display: inline-block; margin: 0 5px 5px 0"
             >
@@ -24,6 +24,9 @@
 <script>
 export default {
     name: 'AppBlogGallery',
+    props: ["blogDetails"],
+    components: {
+    },
     data () {
         return {
             media: [
@@ -84,12 +87,12 @@ export default {
   transition: all .5s;
   opacity: 0;
   visibility: hidden;
-  border-radius: 15px;
+  border-radius: 12px;
 }
 .side-bar-widget .blog-gallery li a img {
   width: 100%;
   height: 100%;
-  border-radius: 15px;
+  border-radius: 12px;
 }
 .side-bar-widget .blog-gallery li a svg {
   position: absolute;
--- components/blogs/AppBlogsItems.vue
+++ components/blogs/AppBlogsItems.vue
@@ -4,7 +4,6 @@
             <div class="section-title text-center">
                 <span class="sp-color2">Latest Blog</span>
                 <h2>Let’s Check Some Latest Blog</h2>
-                
             </div>
             <div class="row pt-45">
                 <div v-for="blog in blogs.blogs" :key="blog.id" class="col-lg-4 col-md-6" @click="$router.push(`/blog/${blog.id}`)">
@@ -14,8 +13,8 @@
                                 <img :src="blog.category.image" alt="Blog Images">
                             </router-link>
                             <div class="blog-tag">
-                                <h3>11</h3>
-                                <span>Dec</span>
+                                <h3>{{ $date(new Date(blog.publish_date), 'dd') }}</h3>
+                                <span>{{ $date(new Date(blog.publish_date), 'MMM') }}</span>
                             </div>
                         </div>
                         <h3 class="title">
@@ -24,13 +23,13 @@
                         <div class="content">
                             <ul>
                                 <li>
-                                    <a href="#"><font-awesome-icon icon="fa-regular fa-user" />By Admin</a>
+                                    <a href="#"><font-awesome-icon icon="fa-regular fa-user" />By {{blog.username}}</a>
                                 </li>
                                 <li>
                                     <a href="index.html"><font-awesome-icon icon="fa-solid fa-tag" />Business</a>
                                 </li>
                             </ul>
-                            <p>{{blog.description}}</p>
+                            <p>{{blog.short_description}}</p>
                         </div>
                     </div>
                 </div>
@@ -68,7 +67,7 @@ export default {
     }
     .blog-area .section-title h2 {
         max-width: 600px;
-        color: #212934;
+        color: #212529;
         font-size: 35px;
         font-weight: 800;
         letter-spacing: -1px;
@@ -90,7 +89,7 @@ export default {
         -webkit-box-shadow: 0 0 15px rgba(0,0,0,.05);
         box-shadow: 0 0 15px rgba(0,0,0,.05);
         margin-bottom: 30px;
-        border-radius: 15px;
+        border-radius: 12px;
         -webkit-transition: .9s;
         transition: .9s;
         border: 1px solid #f1f1f1;
@@ -111,7 +110,7 @@ export default {
         margin: 0 0 0;
     }
     .blog-card .blog-img img {
-        border-radius: 15px 15px 0 0;
+        border-radius: 12px 12px 0 0;
         height: 250px;
         width: 100%;
     }
@@ -122,7 +121,7 @@ export default {
         text-align: center;
         background-color: var(--main-color);
         padding: 15px 12px;
-        border-radius: 15px;
+        border-radius: 12px;
         text-align: center;
     }
     .blog-card:hover .blog-img .blog-tag {
@@ -132,7 +131,7 @@ export default {
         text-align: center;
         background-color: var(--main-color);
         padding: 15px 12px;
-        border-radius: 15px;
+        border-radius: 12px;
         text-align: center;
     }
     .blog-card .blog-img .blog-tag h3 {
@@ -149,7 +148,7 @@ export default {
         font-weight: 500;
     }
     .blog-card .content ul li a {
-        color: var(--main-color);
+        color: #212529;
     }
     .blog-card .content ul li a svg{
         font-size: 20px;
