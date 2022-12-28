7eb22a57255d0d9ae85cacd8aa361601b5978591
civil.ahmad.eid <civil.ahmad.eid@gmail.com>
2022-12-28 19:26:57 +0200

    add cart pages

--- components/header/AppHeader.vue
+++ components/header/AppHeader.vue
@@ -3,6 +3,18 @@
     :class="!topOfPage ? 'onScroll' : ''"
     class="header header-style-2 clearfix"
   >
+    <div class="cart" :class="openCart ? 'opened' : ''">
+      <div class="head">
+        <i class="fa-regular fa-xmark" @click="openCart = false"></i>
+        <button
+          @click="goToCheckout"
+          :disabled="$store.state.cartItems.length <= 0"
+        >
+          <i class="fa-regular fa-badge-check"></i> Checkout
+        </button>
+      </div>
+      <cart />
+    </div>
     <div class="row m-0 w-100">
       <b-navbar toggleable="lg">
         <b-navbar-brand href="/">
@@ -50,6 +62,10 @@
               <i class="fa-solid fa-angle-right"></i>
             </a>
             <lang-switch></lang-switch>
+            <div class="m-0 cartIcon" @click="openCart = !openCart">
+              <span>{{ $store.state.cartItems.length }}</span>
+              <i class="fa-regular fa-cart-plus"></i>
+            </div>
             <div v-if="$store.state.user" class="logout" @click="logout">
               <i class="fa-regular fa-right-from-bracket"></i>
             </div>
@@ -61,16 +77,19 @@
 </template>
 
 <script>
+import cart from "../cart/cart.vue";
 import langSwitch from "../langSwitch/langSwitch.vue";
 export default {
   name: "AppHeader",
   data() {
     return {
       topOfPage: true,
+      openCart: false,
     };
   },
   components: {
     langSwitch,
+    cart,
   },
   beforeMount() {
     window.addEventListener("scroll", this.handleScroll);
@@ -90,6 +109,10 @@ export default {
         if (!this.topOfPage) this.topOfPage = true;
       }
     },
+    goToCheckout() {
+      this.openCart = false;
+      this.$router.push("/checkout");
+    },
   },
 };
 </script>
@@ -101,6 +124,100 @@ header {
   z-index: 10;
   background-color: rgb(234, 54, 46);
   position: fixed;
+  .cart {
+    width: 390px;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    right: 0;
+    transform: translateX(390px);
+    background-color: #fff;
+    z-index: 999999;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
+    .head {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 10px;
+      & > i {
+        border: 1px solid var(--main-color);
+        border-radius: 5px;
+        width: 30px;
+        height: 30px;
+        display: grid;
+        place-items: center;
+        cursor: pointer;
+        background-color: var(--main-color);
+        color: #fff;
+        &:hover {
+          color: var(--main-color);
+          background: transparent;
+        }
+      }
+      button {
+        padding: 5px 30px;
+        font-size: 1.1rem;
+        background-color: var(--main-color);
+        color: #fff;
+        border: 1px solid var(--main-color);
+        display: flex;
+        align-items: center;
+        gap: 5px;
+        i {
+          font-size: 1.1rem;
+        }
+        &:disabled {
+          opacity: 0.5;
+          cursor: not-allowed;
+          &:hover {
+            background-color: var(--main-color);
+            color: #fff;
+          }
+        }
+        &:hover {
+          background-color: transparent;
+          color: var(--main-color);
+        }
+      }
+    }
+    &.opened {
+      transform: translateX(0);
+    }
+  }
+  .cartIcon {
+    border: 1px solid #fff;
+    border-radius: 5px;
+    width: 45px;
+    height: 45px;
+    display: grid;
+    place-items: center;
+    cursor: pointer;
+    position: relative;
+    span {
+      position: absolute;
+      top: -15px;
+      right: -10px;
+      width: 30px;
+      height: 30px;
+      background-color: var(--main-color);
+      border-radius: 50%;
+      color: #fff;
+      display: grid;
+      place-content: center;
+      font-size: 1.2rem;
+      border: 1px solid #fff;
+    }
+    i {
+      color: #fff;
+    }
+    &:hover {
+      background-color: var(--main-color);
+      border-color: var(--main-color);
+      i {
+        color: #fff;
+      }
+    }
+  }
 }
 
 .logout {
